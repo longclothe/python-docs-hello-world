3595320788fc097c9275ec98031936c2c259a5bf
longclothe <504429164@qq.com>
2021-06-10 21:57:41 +0800

    upload_3

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def upload():
     if request.method == 'POST':
         f = request.files['file']
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
-        upload_path = os.path.join(basepath, 'static\uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        upload_path = os.path.join(basepath,'static/uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
         return redirect(url_for('upload'))
     return render_template('upload.html')
