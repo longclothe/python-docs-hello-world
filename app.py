91e259454103a8536a7c6aadaf88368ddf6970a9
longclothe <504429164@qq.com>
2021-06-11 11:29:04 +0800

    quiz1

--- app.py
+++ app.py
@@ -7,7 +7,8 @@
 # coding:utf-8
 
 from flask import Flask,render_template,request,redirect,url_for
-from werkzeug.utils import secure_filename
+from numpy.core.numeric import NaN
+from werkzeug.utils import escape, secure_filename
 import os
 import pandas as pd
 
@@ -18,15 +19,19 @@ def first():
     df = pd.read_csv('static/uploads/names.csv')
     df.Picture = "<img src = \"static/uploads/"+df['Picture']+"\"height=\"100\" width = \"100\"/>"
 
-    return df.to_html(escape=False)
+    df.insert(df.shape[1],"Change",["" for i in range(df.shape[0])])
+    
+    for i in range(df.shape[0]):
+        df["Change"][i]="<a href=\"/change_grade_picture?name="+"\'"+df['Name'][i]+"\'"+"\"><button>Click it !</button></a>"
+    return "<font size=\"6\" color=\"red\">This is 1001889074 Yangfan Chen</font>"+"<br>"+df.to_html(escape=False)
     # return render_template('index.html')
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         f = request.files['select']
-        basepath = os.path.dirname(__file__)  # 当前文件所在路径
-        upload_path = os.path.join(basepath,'static/uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        basepath = os.path.dirname(__file__)  
+        upload_path = os.path.join(basepath,'static/uploads',secure_filename(f.filename))  
         f.save(upload_path)
         return redirect(url_for('upload'))
     names = str(os.listdir('static/uploads'))
@@ -44,14 +49,18 @@ def search():
     # key = request.args.get('wd')
     # print(key)
 
-	return "1001889074 Yangfan Chen"+"<br>"+render_template('search.html')
+	return "<font size=\"6\" color=\"red\">This is 1001889074 Yangfan Chen</font>"+"<br>"+render_template('search.html')
 
 @app.route('/sp')
 def get_result():
     key = request.args.get('wd')
     data = pd.read_csv("static/uploads/names.csv")
-    data = data.loc[data['Room']==int(key)]
-    return data.to_html()
+    # data = data.loc[data['State']==key]
+    data = data[data.Caption.contains(key)]
+
+    data.Picture = data['Picture']+"<img src = \"static/uploads/"+data['Picture']+"\"height=\"100\" width = \"100\"/>"
+
+    return data.to_html(escape=False)
     # return get_html(key)
 
 @app.route('/search_grade')
@@ -75,6 +84,100 @@ def get_grade():
     
 #     form = forms.SearchForm()
 #     return render_template('auto_test_case.html', cases=auto_test_case_objs,form=form)
+
+@app.route('/change_grade_picture',methods=['POST', 'GET'])
+def change_grade_picture():
+    name=request.args.get('name').strip('\'')
+    # print(name)
+    if request.method == 'POST':
+        new_points = request.form.get('points')
+        new_picture = request.form.get('pic_name')
+        df = pd.read_csv("static/uploads/names.csv")
+        df.Grade[df['Name']==name] = new_points
+        df.Picture[df['Name']==name] = new_picture
+        df.to_csv("static/uploads/names.csv",index=False)
+        # df = pd.read_csv("static/uploads/names.csv",index_col=False)
+        return redirect(url_for('first'))
+
+    return render_template('change_grade_picture.html')
+
+@app.route('/search_picture')
+def search_picture():
+
+
+    return "<font size=\"6\" color=\"red\">This is 1001889074 Yangfan Chen</font>"+"<br>"+render_template('search_picture.html')
+
+@app.route('/picture_result')
+def get_picture_result():
+    key = request.args.get('name')
+    data = pd.read_csv("static/uploads/names.csv")
+    data = data.loc[data['Name']==key]
+    
+    # data.insert(data.shape[1],"Picture_Name",["" for i in range(data.shape[0])])
+
+    # for i in range(data.shape[0]):
+        
+    #     data["Picture_Name"][i]=data['Picture'][i]
+
+    data.Picture = data['Picture']+"<img src = \"static/uploads/"+data['Picture']+"\"height=\"100\" width = \"100\"/>"
+
+    # data.insert(data.shape[1],"Picture_Name",["" for i in range(data.shape[0])])
+
+
+
+    # for i in range(data.shape[0]):
+    #     if data["Picture"][i]== NaN:
+    #         data["Picture_Name"][i]=NaN
+    #     else:
+    #         data["Picture_Name"][i]=data['Picture'][i]
+    return "<font size=\"6\" color=\"red\">This is 1001889074 Yangfan Chen</font>"+"<br>"+data.to_html(escape=False)
+
+
+@app.route('/search_room')
+def search_room():
+
+
+    return "<font size=\"6\" color=\"red\">This is 1001889074 Yangfan Chen</font>"+"<br>"+render_template('search_room.html')
+
+
+@app.route('/room_result')
+def get_room_result():
+    key = request.args.get('room')
+    data = pd.read_csv("static/uploads/names.csv")
+    # data = data.loc[data['Room']==int(key)]
+    
+
+
+    data.Picture = "<img src = \"static/uploads/"+data['Picture']+"\"height=\"100\" width = \"100\"/>"
+    
+
+    data.insert(data.shape[1],"Change",["" for i in range(data.shape[0])])
+    
+    for i in range(data.shape[0]):
+        data["Change"][i]="<a href=\"/change_room_number?name="+"\'"+data['Name'][i]+"\'"+"\"><button>Change Room Number !</button></a>"
+    data = data.loc[data['Room']==int(key)]
+
+    return "<font size=\"6\" color=\"red\">This is 1001889074 Yangfan Chen</font>"+"<br>"+data.to_html(escape=False)
+
+@app.route('/change_room_number',methods=['POST', 'GET'])
+def change_room_number():
+    name=request.args.get('name').strip('\'')
+    # print(name)
+    if request.method == 'POST':
+        new_number = request.form.get('new_number')
+        # new_picture = request.form.get('pic_name')
+        data = pd.read_csv("static/uploads/names.csv")
+        data = data.loc[data['Name']==name]
+        data.Room[data['Name']==name] = new_number
+      
+        data.to_csv("static/uploads/names.csv",index=False)
+        # df = pd.read_csv("static/uploads/names.csv",index_col=False)
+        return redirect(url_for('search_room'))
+
+    return render_template('change_room_number.html')
+
+
+
 
 
 if __name__ == '__main__':
